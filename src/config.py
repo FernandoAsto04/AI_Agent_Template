747e9cf4b3bc3f6fda99814e222684e9fdaa05b9
FernandoAsto04 <20214543@aloe.ulima.edu.pe>
2026-08-20 10:38:47 -0500

    Se crearon mas rutas dentro del config.py

--- src/config.py
+++ src/config.py
@@ -4,6 +4,9 @@ DIR_SRC = Path(__file__).resolve().parent
 RUTA_RAIZ = DIR_SRC.parent
 
 R_AGENT = RUTA_RAIZ/"agent"
-
+R_MEMORY = RUTA_RAIZ/"memory"
+R_MODELS = RUTA_RAIZ/"models"
+R_PROTOCOL = RUTA_RAIZ/"protocol"
+R_TOOLS = RUTA_RAIZ/"tools"
 
 R_SYSTEM_PROMPT = R_AGENT/"system_prompt.md"
